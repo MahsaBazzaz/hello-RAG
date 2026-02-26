fe7082b5342437344c38508a937728e8a351c090
MahsaBazzaz <mahsa.bazzaz@gmail.com>
2026-02-26 11:00:05 -0500

    test harder tool calling

--- tool_calling.py
+++ tool_calling.py
@@ -1,7 +1,11 @@
 from dotenv import load_dotenv
+from typing import List
+from pydantic import BaseModel, Field
+
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.tools import tool
 from langchain.agents import create_agent
+from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
 
@@ -13,13 +17,26 @@ def multiply(x: float, y: float) -> float:
     """Multiply 'x' times 'y'."""
     return x * y
 
+@tool
+def sum(x: float, y: float) -> float:
+    """Sum 'x' and 'y'."""
+    return x + y
+
+class Source(BaseModel):
+    """ Scheme used for source used by the agent."""
+    url: str = Field(description="URL of the source")
+
+class AgentResponse(BaseModel):
+    """ Scheme used for agent response."""
+    answer: str = Field(description="Agent's answer to the question")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to answer the question")
 
 if __name__ == "__main__":
     print("Hello Tool Calling")
 
-    tools = [TavilySearchResults(), multiply]
-    llm = ChatOpenAI(model="gpt-4-turbo", temperature=0)
-
+    tools = [TavilySearchResults(), multiply, sum]
+    # llm = ChatOpenAI(model="gpt-4-turbo", temperature=0)
+    llm = ChatOllama(model = "llama3.1:8b", temperature=0)
     agent = create_agent(
         model=llm,
         tools=tools,
@@ -27,13 +44,14 @@ if __name__ == "__main__":
     You are a weather assistant.
     Use tools when necessary.
     Always respond in Celsius.
-    """
+    """,
+    response_format=AgentResponse
     )
 
     res = agent.invoke(
         {
         "messages": [
-            {"role": "user", "content": "What is the weather in Dubai right now? Compare it with San Francisco. Output in Celsius."}
+            {"role": "user", "content": "Give me the summation of weather temperatures for Boston right now and San Francisco right now. Output in Celsius."}
         ]
         }
     )
